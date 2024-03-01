cd7cd64fc3cf158edeb6c200b963d045f5fb8519
Qadir-Bux-734 <160654196+Qadir-Bux-734@users.noreply.github.com>
2024-03-01 18:04:04 +0500

    Add files via upload

---
+++ Q5/Q5.js
@@ -0,0 +1,5 @@
+// 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+//Soloution:
+var famous_person = "Hazrat Ali (RA) Said";
+var message = "The tongue is like a lion, if you let it loose, it will wound someone.";
+console.log("".concat(famous_person, "once said,\u201C ").concat(message, "\u201D"));
