832145d74b113ad54623d92172e4c67908d58657
Qadir-Bux-734 <160654196+Qadir-Bux-734@users.noreply.github.com>
2024-03-09 16:30:41 +0500

    Add files via upload

---
+++ Q8/ex_08.ts
@@ -0,0 +1,12 @@
+/* 8. You should create four lines that look like this:
+ console.log(5 + 3)
+ Your output should simply be four lines with the number 8 appearing once on each line.
+*/
+    
+
+
+ console.log(4 + 4); // 8 
+ console.log(33 - 25); // 8 
+ console.log(2 * 4); // 8 
+ console.log(56 / 7 ); // 8 
+ 
