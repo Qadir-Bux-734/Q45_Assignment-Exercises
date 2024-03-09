3074385feea6a734b568a55b51555464bb8c64dd
Qadir-Bux-734 <160654196+Qadir-Bux-734@users.noreply.github.com>
2024-03-09 11:41:14 +0500

    Add files via upload

---
+++ Q6/Q6.ts
@@ -0,0 +1,13 @@
+
+// 6. Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each
+// character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+let storeMan:string= " \tAhmed Khan"
+console.log(storeMan)
+// \t  In programming, "\t" represents the horizontal tab character. It is commonly used to insert horizontal tab spaces in strings and text output.
+   let person_name:string= "\t\tQadir Bux "
+     console.log(person_name)
+
+ //\n    In programming, "\n" represents the newline character. It is commonly used to insert a line break or start a new line of text.
+    let firstName:string= "\n  Ali Raza"
+      console.log(firstName);
