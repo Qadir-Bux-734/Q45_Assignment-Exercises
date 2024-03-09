105dbf91a1ab88468f015aa73b5fdc0ed715a196
Qadir-Bux-734 <160654196+Qadir-Bux-734@users.noreply.github.com>
2024-03-09 16:35:39 +0500

    Add files via upload

---
+++ Q13/Q13.ts
@@ -0,0 +1,11 @@
+// Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
+// to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+let myTransporation:string[]=["motorcycle","Bus","Train", "Vagin" ,"car"]
+
+console.log(`I would like to own a ${myTransporation[0]}.`);
+console.log(`I would like to own a ${myTransporation[1]}.`);
+console.log(`I would like to own a ${myTransporation[2]}.`);
+console.log(`I would like to own a ${myTransporation[3]}.`);
+console.log(`I would like to own a ${myTransporation[4]}.`);
+
