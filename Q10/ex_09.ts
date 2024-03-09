ee56b773ec60d7126abd1b1a21cfc9ef40bc8521
Qadir-Bux-734 <160654196+Qadir-Bux-734@users.noreply.github.com>
2024-03-09 16:32:49 +0500

    Add files via upload

---
+++ Q10/ex_09.ts
@@ -0,0 +1,14 @@
+
+/*10. Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write
+because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence
+describing what the program does.
+*/
+
+
+const n1:number=56;
+const n2:number=23;
+
+// This will add the n1 and n2 (this is a comment in Typescript.)
+  console.log(n1 + n2);
+
+
