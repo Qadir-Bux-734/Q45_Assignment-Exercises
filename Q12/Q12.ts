f7d278dc39e70c55817977f52d5037c19397f813
Qadir-Bux-734 <160654196+Qadir-Bux-734@users.noreply.github.com>
2024-03-09 16:34:47 +0500

    Add files via upload

---
+++ Q12/Q12.ts
@@ -0,0 +1,11 @@
+// Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message
+// should be the same, but each message should be personalized with the person’s name.
+
+
+
+const friendsName:string[]=["Qadir","Uzair","Ali","Raza","Zulfiqar"]
+console.log( `Hi ${friendsName[0]} ,Are you fine?`  );
+console.log( `Hi ${friendsName[1]} ,Are you fine?`  );
+console.log( `Hi ${friendsName[2]} ,Are you fine?`  );
+console.log( `Hi ${friendsName[3]} ,Are you fine?`  );
+console.log( `Hi ${friendsName[4]} ,Are you fine?`  );
