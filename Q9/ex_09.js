49990346d6e0f37e406530ab08cd0eb61304fde1
Qadir-Bux-734 <160654196+Qadir-Bux-734@users.noreply.github.com>
2024-03-09 16:31:18 +0500

    Add files via upload

---
+++ Q9/ex_09.js
@@ -0,0 +1,5 @@
+/* 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+*/
+var favNum = 7;
+var message = "Hello, This is My lucky Number  \u201C".concat(favNum, "\u201D");
+console.log(message);
---
+++ Q9/ex_09.ts
@@ -0,0 +1,5 @@
+ /* 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+*/
+    let favNum:number=  7;
+    let message:string =`Hello, This is My lucky Number  “${favNum}”`;
+    console.log(message);
