a73b24c57818ae82d0f9ef2bb7b2e110a94368f9
Qadir-Bux-734 <160654196+Qadir-Bux-734@users.noreply.github.com>
2024-03-09 16:29:49 +0500

    Add files via upload

---
+++ Q7/ex_07.ts
@@ -0,0 +1,9 @@
+/*7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results. */
+
+
+let addition:number=(3 + 5); 
+let subtraction:number=(16 - 8);
+let multiplication:number=(2 * 4);
+let division:number=(64 / 8);
+  console.log(addition,subtraction,multiplication,division);
+  
